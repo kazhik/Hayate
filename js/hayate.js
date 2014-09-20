444aef6c257ea1c411fe3523d6a7c5ee3ca8eec4
Koike Kazuhiko <kazhik@gmail.com>
2014-09-20 10:23:15 +0900

    removed unnecessary check

--- js/hayate.js
+++ js/hayate.js
@@ -35,8 +35,9 @@ Hayate.start = function() {
         initUI();
         
         console.log("Hayate started");
-    
+
     }
+
     function onFail(e) {
         console.log(e);
     }
@@ -52,55 +53,6 @@ Hayate.start = function() {
             
         return dfd.promise();
         
-    }
-    
-    if (typeof Hayate.Config === "undefined") {
-        console.log("Hayate.Config undefined");
-        return;
-    }
-    if (typeof Hayate.Database === "undefined") {
-        console.log("Hayate.Database undefined");
-        return;
-    }
-    if (typeof Hayate.Recorder === "undefined") {
-        console.log("Hayate.Recorder undefined");
-        return;
-    }
-    if (typeof Hayate.WatchView === "undefined") {
-        console.log("Hayate.WatchView undefined");
-        return;
-    }
-    if (typeof Hayate.MapView === "undefined") {
-        console.log("Hayate.MapView undefined");
-        return;
-    }
-    if (typeof Hayate.StorageView === "undefined") {
-        console.log("Hayate.StorageView undefined");
-        return;
-    }
-    if (typeof Hayate.LogView === "undefined") {
-        console.log("Hayate.LogView undefined");
-        return;
-    }    
-    if (typeof Hayate.ConfigView === "undefined") {
-        console.log("Hayate.ConfigView undefined");
-        return;
-    }
-    if (typeof Hayate.RecordsView === "undefined") {
-        console.log("Hayate.RecordsView undefined");
-        return;
-    }
-    if (typeof Hayate.LapsView === "undefined") {
-        console.log("Hayate.LapsView undefined");
-        return;
-    }
-    if (typeof Hayate.PanelView === "undefined") {
-        console.log("Hayate.PanelView undefined");
-        return;
-    }
-    if (typeof Hayate.ViewUtil === "undefined") {
-        console.log("Hayate.ViewUtil undefined");
-        return;
     }
     
     var dbInfo = {
